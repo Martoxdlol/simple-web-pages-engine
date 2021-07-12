0026501e0fe8c374f68305f83c2f4f8c609626bb
Tomás Cichero <tomascichero@gmail.com>
2021-07-12 15:31:39 -0300

    new render method that ACTUALLY WORKS!
    Changed htmlparser2 to node-html-parser

--- src/process_file.js
+++ src/process_file.js
@@ -1,64 +1,38 @@
-const htmlparser2 = require("htmlparser2");
-const selfClosing = new Set([
-    'area',
-    'base',
-    'col',
-    'embed',
-    'hr',
-    'br',
-    'img',
-    'input',
-    'link',
-    'meta',
-    'param',
-    'source',
-    'track',
-    'wbr',
-    'command',
-    'keygen',
-    'menuitem',
-])
+const nodeHtmlParser = require("node-html-parser")
+const { joinUrl, relativeUrlParts, getUrlDirPath } = require("./util")
 
-class Template{
-    
-}
-
-function makeTemplate(content){
-    const list = []
-    const keys = {}
-    let html = ''
-    const parser = new htmlparser2.Parser({
-        onopentag(name, attributes) {
-            html += `<${name}`
-            for(k in attributes){
-                if(!attributes[k]) html += ' '+k
-                else html += ` ${k}="`+escapeHtml(attributes[k])+'"'
+async function _render(code, props, children, fileSystem, relativePath){
+    const root = nodeHtmlParser.parse(code)
+    const includes = root.querySelectorAll("include")
+    const childrenElem = root.querySelector('children')
+    if(childrenElem){
+        if(children){
+            childrenElem.parentNode.exchangeChild(childrenElem, children)
+        }else{
+            childrenElem.parentNode.removeChild(childrenElem)
+        }     
+    }
+    for(const elem of includes){
+        const src = elem.getAttribute('src')
+        if(src){
+            const srcPath = joinUrl(relativePath, src)
+            const dirPath = getUrlDirPath(srcPath)
+            const newElemRaw = await fileSystem.getFileContent(srcPath)
+            if(newElemRaw){
+                elem.parentNode.exchangeChild(
+                    elem,
+                    await _render(newElemRaw, elem.attributes, elem.childNodes, fileSystem, dirPath)
+                )
+            }else{
+                console.error(`${srcPath} not found`)
             }
-            if(selfClosing.has(name)) html += '/>'
-            else  html += '>'
-        },
-        ontext(text) {
-            html += text
-        },
-        onclosetag(tagname) {
-            if(!selfClosing.has(tagname)) html += `</${tagname}>`
-        },
-    })
-    parser.write(content)
-    parser.end()
-    return html
+        }
+    }
+    return root
 }
 
-function escapeHtml(text) {
-    var map = {
-      '&': '&amp;',
-      '<': '&lt;',
-      '>': '&gt;',
-      '"': '&quot;',
-      "'": '&#039;'
-    };
-    
-    return text.replace(/[&<>"']/g, function(m) { return map[m]; });
-  }
+async function render(html, fileSystem){
+    return (await _render(html, {}, null, fileSystem, '/')).toString()
+}
 
-exports.makeTemplate = makeTemplate 
+exports.render = render 
